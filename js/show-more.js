0a39666152eb3eabf4b223dbf1bd36c9fee1a5a0
Nazar Zhuvaka <nazar.zhuvaka.mcboss@gmail.com>
2024-03-28 16:21:06 +0300

    commit

---
+++ js/show-more.js
@@ -0,0 +1,17 @@
+const showMore = document.querySelector('.show-more');
+const productsLength = document.querySelectorAll('.cards__item').length;
+
+let items = 4;
+
+showMore.addEventListener('click' , () => {
+    items += 4;
+    const array = Array.from(document.querySelector('.card-products').children);
+
+    const visItems = array.slice(0, items);
+
+    visItems.forEach(el => el.classList.add('is-visible'));
+
+    // if (visItems.length === productsLength ) {
+    //     showMore.style.display = 'none'
+    // }
+})
