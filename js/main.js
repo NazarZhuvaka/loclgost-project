a8e231f1f0ef4b87688fbb9f6533eef4d46e17b0
Nazar Zhuvaka <nazar.zhuvaka.mcboss@gmail.com>
2024-03-28 09:58:49 +0300

    tabs

--- js/main.js
+++ js/main.js
@@ -18,4 +18,37 @@ window.onload = function() {
         document.getElementById("year-clock").innerHTML = clock; 
     }
     );
+};
+
+
+// Tabs
+
+const tabsBtns = document.querySelectorAll("[data-tab]");
+const tabsProducts = document.querySelectorAll("[data-tab-value]");
+
+for (let btn of tabsBtns) {
+  btn.addEventListener("click", function () {
+    //убрати актів у всі
+    for (let btn of tabsBtns) {
+      btn.classList.remove("tab-controls__btn--active");
+    }
+    //добавити активний клас до данного кнопки
+    this.classList.add("tab-controls__btn--active");
+
+    // відобразити потрібні товари і hide непотрібні
+    for (let product of tabsProducts) {
+      //провірка на all
+      if (this.dataset.tab === "all") {
+        product.classList.remove("none");
+      } else {
+        if (product.dataset.tabValue === this.dataset.tab) {
+          product.classList.remove("none");
+        } else {
+          product.classList.add("none");
+        }
+      }
+    }
+
+    
+  });
 }
