fc4bd1b2d47c52a4251e47416a5f560a1ba075c7
Nazar Zhuvaka <nazar.zhuvaka.mcboss@gmail.com>
2024-03-29 15:05:04 +0300

    animations

--- js/animation.js
+++ js/animation.js
@@ -28,7 +28,7 @@ function animateTitle() {
       span.classList.add("green-word"); // Додаємо клас для зеленого слова
     }
     span.textContent = word;
-    span.style.animationDelay = `${0.4 * index}s`; // Затримка анімації
+    span.style.animationDelay = `${0.3 * index}s`; // Затримка анімації
     title.appendChild(span);
 
     if (index < words.length - 1) {
@@ -36,3 +36,45 @@ function animateTitle() {
     }
   });
 }
+
+
+
+// kinks 
+
+// Отримання елементів підсекцій
+var navItems = document.querySelectorAll('.links__nav');
+
+// Функція, яка відслідковує скрол та запускає анімацію, коли підсекція наближається до видимості
+function checkPosition() {
+  for (var i = 0; i < navItems.length; i++) {
+    var positionFromTop = navItems[i].getBoundingClientRect().top;
+    if (positionFromTop - window.innerHeight < 0) {
+      navItems[i].classList.add('animate');
+    }
+  }
+}
+
+// Виклик функції при прокрутці
+window.addEventListener('scroll', checkPosition);
+// Виклик функції один раз при завантаженні сторінки
+window.addEventListener('DOMContentLoaded', checkPosition);
+
+
+// 
+
+document.addEventListener("DOMContentLoaded", function() {
+  const cards = document.querySelectorAll('.card');
+
+  function revealCards() {
+    cards.forEach(function(card, index) {
+      setTimeout(function() {
+        card.classList.add('active');
+      }, index * 200); // Затримка у мілісекундах
+    });
+  }
+
+  revealCards();
+});
+
+
+
--- js/map.js
+++ js/map.js
@@ -1,9 +1,27 @@
 function initMap() {
-    let uluru = {lat: 43.74713715504195, lng: -79.36926801205719,}
+    let сabin = {lat: 43.74713715504195, lng: -79.36926801205719,}
+
+    let loft = {lat: 42.84316337289665,  lng: -80.32868034393724,}
+
+    let mark = {lat: 43.87770517831332,   lng: -79.3245549803741,}
+
+    let mic = {lat: 43.618623365006165,    lng: -79.67843631581329,}
+
+
+
     let map = new google.maps.Map (
-      document.getElementById('map') , {zoom:8 , center:uluru}
+      document.getElementById('map') , {zoom:7 , center:сabin}
     );
-    let marker = new google.maps.Marker({position:uluru, map: map})
+
+
+    let marker = new google.maps.Marker({position:сabin, map: map})
+
+    let marker1 = new google.maps.Marker({position:loft, map: map})
+
+    let marker2 = new google.maps.Marker({position:mark, map: map})
+
+    let marker3 = new google.maps.Marker({position:mic, map: map})
+
   }
 
 
